3d056bb9b3905207fa841c5b64b7a2f17ec7ed96
sagnikc395 <sagnikchatterjee607@gmail.com>
2026-06-19 15:56:45 -0400

    refactor: refactor the config to dataclasses to make it immutable

--- gaiaagent/config.py
+++ gaiaagent/config.py
@@ -1,9 +1,14 @@
-API_URL = "https://agentsc-course-unit4-scoring.hf.space"
+from dataclasses import dataclass
 
+@dataclass(frozen=True)
+class GAIAAgentConfig:
+    API_URL = "https://agentsc-course-unit4-scoring.hf.space"
 
-## model config
-# since using groq,using gpt-oss-120b for inference 
-MODEL_ID = "openai/gpt-oss-120b"
+    ## model config
+    # since using groq,using gpt-oss-120b for inference 
+    MODEL_ID = "openai/gpt-oss-120b"
 
-## max retries for
-MAX_RETRY = 5
+    ## max retries for
+    MAX_RETRY = 5
+
+config = GAIAAgentConfig()
